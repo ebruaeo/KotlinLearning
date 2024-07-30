51e71e1b690e1a21ca408081c4d0ba590bfdb0ae
ebruaeo <ebruaeo@hotmail.com>
2024-07-30 12:12:38 +0300

    function exercise

--- src/Functions.kt
+++ src/Functions.kt
@@ -1,10 +1,25 @@
 fun main() {
-    for (i in 1..3){
-        sayHello()
+    for (i in 1..3) {
+//        multiply()
+        askNameAndBirthYear()
     }
+
+
 }
 
-fun sayHello(){
-    println("Hello Everyone")
-}
+//fun multiply() {
+//    println("Enter a number")
+//    val input = readLine() ?: ""
+//    val number = input.toInt()
+//    println("$number * 17 = ${number * 17}")
+//}
 
+fun askNameAndBirthYear() {
+    println("name:")
+    var input = readLine() ?: ""
+    val name = input.toString()
+    println("Birth Year:")
+    input = readLine() ?: ""
+    val year = input.toInt()
+    println("name: $name and birth year: $year")
+}
