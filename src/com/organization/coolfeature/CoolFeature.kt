7aaeb62b73c29809badd4330c1a22cc6a4303132
ebruaeo <ebruaeo@hotmail.com>
2024-08-01 14:39:25 +0300

    packages

---
+++ src/com/organization/coolfeature/CoolFeature.kt
@@ -0,0 +1,9 @@
+package com.organization.coolfeature
+
+fun buildCoolFeature(){
+    println("Building a cool feature")
+}
+
+fun testCoolFeature(){
+    println("Testing a cool feature")
+}
---
+++ src/com/organization/main/Packages.kt
@@ -0,0 +1,7 @@
+package com.organization.main
+
+import com.organization.coolfeature.buildCoolFeature
+
+fun main(args:Array<String>) {
+buildCoolFeature()
+}
