d5527b6676fdb0c8b9ad99753db48199d9ea6b4d
ebruaeo <ebruaeo@hotmail.com>
2024-08-03 14:25:43 +0300

    player movement

--- src/Main.kt
+++ src/Main.kt
@@ -10,13 +10,35 @@ fun main(args: Array<String>) {
     }
 
     printBoard()
+
+    var continueGame = true
+    do {
+        println("Please enter a position(e.g. 1,1)")
+        val input = readLine() ?: ""
+        var x = 0
+        var y = 0
+        try {
+            val positions = input.split(",")
+            x = positions[0].trim().toInt()
+            y = positions[1].trim().toInt()
+
+            if (board[x - 1][y - 1] != "") {
+                println("That position is already taken")
+            } else {
+                board[x - 1][y - 1] = "X"
+                printBoard()
+            }
+        } catch (e: Exception) {
+            println("Invalid input please try again")
+        }
+    } while (continueGame)
 }
 
 fun printBoard() {
     println("-------------")
-    for(i in 0..2) {
-        for(j in 0..2) {
-            when(board[i][j]) {
+    for (i in 0..2) {
+        for (j in 0..2) {
+            when (board[i][j]) {
                 "X" -> print("| X ")
                 "O" -> print("| O ")
                 else -> print("|   ")
