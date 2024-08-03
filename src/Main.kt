d5c87821e131c26743ac10b5fe23c75736132fde
ebruaeo <ebruaeo@hotmail.com>
2024-08-03 14:18:26 +0300

    Update the board

--- src/Main.kt
+++ src/Main.kt
@@ -13,16 +13,16 @@ fun main(args: Array<String>) {
 }
 
 fun printBoard() {
-    println("----------------")
-    for (i in 0..2) {
-        for (j in 0..2) {
-            when (board[i][j]) {
-                "x" -> print("| x ")
-                "o" -> print("| o ")
-                "else" -> print("|  ")
+    println("-------------")
+    for(i in 0..2) {
+        for(j in 0..2) {
+            when(board[i][j]) {
+                "X" -> print("| X ")
+                "O" -> print("| O ")
+                else -> print("|   ")
             }
         }
         println("|")
-        println("----------------")
+        println("-------------")
     }
 }
