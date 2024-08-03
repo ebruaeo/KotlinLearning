ec882f88357a92d8ecadc25c75039b9388a3d33e
ebruaeo <ebruaeo@hotmail.com>
2024-08-03 14:16:34 +0300

    Create the board

--- src/Main.kt
+++ src/Main.kt
@@ -1,2 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+var board = arrayListOf<ArrayList<String>>()
+
+
+fun main(args: Array<String>) {
+    for (i in 0..2) {
+        val row = arrayListOf<String>()
+        for (j in 0..2)
+            row.add("")
+        board.add(row)
+    }
+
+    printBoard()
+}
+
+fun printBoard() {
+    println("----------------")
+    for (i in 0..2) {
+        for (j in 0..2) {
+            when (board[i][j]) {
+                "x" -> print("| x ")
+                "o" -> print("| o ")
+                "else" -> print("|  ")
+            }
+        }
+        println("|")
+        println("----------------")
+    }
+}
