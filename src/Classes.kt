a46170e1ab2ffd9955921ff64d173e673d081b03
ebruaeo <ebruaeo@hotmail.com>
2024-08-05 12:43:54 +0300

    class example

---
+++ src/Classes.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+
+}
+
+
+class Car {
+    var model: String? = null
+    var topSpeed = 100
+
+    fun start() {
+        println("Starting the $model")
+    }
+
+    fun drive(speed: Int) {
+        println("Driving at a speed to $speed")
+
+    }
+}
