1475b1acaaf33fcf9c92ee0865576f8a79fe0382
ebruaeo <ebruaeo@hotmail.com>
2024-08-17 00:06:31 +0300

    Interface implementation

--- src/Interfaces.kt
+++ src/Interfaces.kt
@@ -1,14 +1,31 @@
 fun main() {
-val myOven: Oven? = null
-    myOven?.turnOn()
-    myOven?.cook(150)
+    val myOven: Oven = getOven()
+    myOven.turnOn()
+    myOven.cook(150)
+    myOven.turnOff()
 }
 
 interface Oven {
     val tempature: Int
     fun turnOn()
     fun turnOff()
-    fun cook (temp: Int) {
+    fun cook(temp: Int) {
         println("Cooking at $temp")
     }
+}
+
+class Bosch : Oven {
+    override val tempature = 100
+
+    override fun turnOn() {
+        println("Turning on")
+    }
+
+    override fun turnOff() {
+        println("Turning off")
+    }
+}
+
+fun getOven(): Oven{
+    return Bosch()
 }
